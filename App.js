e264d56dc6fa791de72b0a9d406ee33ca79cdabf
muratcansahn <muratcansahin@Murat-MacBook-Air.local>
2024-05-09 20:54:33 +0300

    init project

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import Home from './src/Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <NavigationContainer>
+      <Stack.Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        headerShown: false
+      }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+
+
+   </NavigationContainer>   );
 }
 
 const styles = StyleSheet.create({
