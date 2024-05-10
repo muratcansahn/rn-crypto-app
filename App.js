3da4121447be64fbf5173b56cfd5a72ce153fa9b
muratcansahn <muratcansahin@192.168.1.10>
2024-05-11 01:11:25 +0300

    feat: add active indicator tab on navigation

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Home from "./src/pages/Home";
 import { screenOptions } from "./utils";
 import { HomeIcon, Search, User, CreditCard, PieChart } from "./assets/icons";
 import { useIsFocused } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
 
 function StatisticsScreen() {
     return (
@@ -53,7 +54,12 @@ export default function App() {
                     name="Home"
                     component={Home}
                     options={{
-                        tabBarIcon: ({ focused }) => <HomeIcon color={focused ? "#00BDB0" : "#B3B4B8"} />,
+                        tabBarIcon: ({ focused }) => (
+                            <>
+                                <HomeIcon color={focused ? "#00BDB0" : "#B3B4B8"} />
+                                {focused && <View style={styles.activeTabIndicator} />}
+                            </>
+                        ),
                     }}
                 />
 
@@ -61,31 +67,63 @@ export default function App() {
                     name="Statistics"
                     component={StatisticsScreen}
                     options={{
-                        tabBarIcon: ({ focused }) => <PieChart color={focused ? "#00BDB0" : "#B3B4B8"} />,
+                        tabBarIcon: ({ focused }) => (
+                            <>
+                                <PieChart color={focused ? "#00BDB0" : "#B3B4B8"} />
+                                {focused && <View style={styles.activeTabIndicator} />}
+                            </>
+                        ),
                     }}
                 />
                 <Tab.Screen
                     name="Search"
                     component={SearchScreen}
                     options={{
-                        tabBarIcon: ({ focused }) => <Search color={focused ? "#00BDB0" : "#B3B4B8"} />,
-                    }}
-                />
-                <Tab.Screen
-                    name="Profile"
-                    component={ProfileScreen}
-                    options={{
-                        tabBarIcon: ({ focused }) => <User color={focused ? "#00BDB0" : "#B3B4B8"} />,
+                        tabBarIcon: ({ focused }) => (
+                            <>
+                                <Search color={focused ? "#00BDB0" : "#B3B4B8"} />
+                                {focused && <View style={styles.activeTabIndicator} />}
+                            </>
+                        ),
                     }}
                 />
                 <Tab.Screen
                     name="Payments"
                     component={PaymentsScreen}
                     options={{
-                        tabBarIcon: ({ focused }) => <CreditCard color={focused ? "#00BDB0" : "#B3B4B8"} />,
+                        tabBarIcon: ({ focused }) => (
+                            <>
+                                <CreditCard color={focused ? "#00BDB0" : "#B3B4B8"} />
+                                {focused && <View style={styles.activeTabIndicator} />}
+                            </>
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <>
+                                <User color={focused ? "#00BDB0" : "#B3B4B8"} />
+                                {focused && <View style={styles.activeTabIndicator} />}
+                            </>
+                        ),
                     }}
                 />
             </Tab.Navigator>
         </NavigationContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    activeTabIndicator: {
+        width: "75%",
+        height: 6,
+        borderRadius: 3,
+        backgroundColor: "#00BDB0",
+        position: "absolute",
+        bottom: "8px",
+        alignSelf: "center",
+    },
+});
